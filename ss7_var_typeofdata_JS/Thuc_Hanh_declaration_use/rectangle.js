f3fa23c845108e51ce7a797d930149d29e73bb44
ThanhNghiadn03 <tranthanhnghia.dn03@gmail.com>
2022-04-13 18:19:37 +0700

    Add new things

---
+++ ss7_var_typeofdata_JS/Thuc_Hanh_declaration_use/rectangle.js
@@ -0,0 +1,4 @@
+let width = 20;
+let heigh = 10;
+let area = width*heigh;
+document.write('area = '+ area);
---
+++ ss7_var_typeofdata_JS/Thuc_Hanh_declaration_use/two_number.js
@@ -0,0 +1,4 @@
+let a = prompt('Nhập vào a : ');
+let b = prompt('Nhập vào b : ');
+let sodu = parseInt(a) % parseInt(b);
+document.write('Số dư của a khi chia cho b là : '+ sodu);
