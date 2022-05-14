ff0ba129d0994838a8b5422ecf82d63511dbac92
ThanhNghiadn03 <tranthanhnghia.dn03@gmail.com>
2022-05-14 23:31:39 +0700

    update 14/5/2022

--- ss16_OOP1/BaiTap/BaiTap_MobilePhone.js
+++ ss16_OOP1/BaiTap/BaiTap_MobilePhone.js
@@ -1,68 +1,108 @@
 class Mobile {
-    constructor(name, pin , OnOrOff) {
-        this.pin = pin;
+    constructor(name,pin,inbox,messageSent,draftMess,onOrOff) {
         this.name = name;
-        this.OnOrOff= OnOrOff;
-    }
-
-    checkOnOrOff() {
-        this.decreasePin();
-        if (this.OnOrOff==true) {
-            return 'Điện thoại '+ this.name + ' đang bật';
-        } else {
-            return 'Điện thoại ' +this.name+ ' đang tắt';
-        }
-    }
-    decreasePin() {
-        this.pin = this.pin - 1;
-    }
-    TurnOnOrOff() {
-        this.decreasePin();
-        if (this.OnOrOff==true) {
-            let user = confirm('Điện thoại đang bật , bạn có muốn tắt ?');
-            if (user==true) {
-                return this.OnOrOff = false;
-            } else {
-                return this.OnOrOff = true;
-            }
-        } else {
-            let user = confirm('Điện thoại đang tắt , bạn có muốn bật ?');
-            if (user == true) {
-                return  this.OnOrOff = true;
-            } else {
-                return this.OnOrOff = false;
-            }
-        }
-    }
-
-    charging() {
-        this.decreasePin();
-        let user = '';
-        if (this.getPin() <= 20) {
-            user = confirm('Pin yếu , bạn có muốn sạc điện thoại ?');
-            if (user==true) {
-                return 'Điện thoại '+ this.name +  ' đang sạc';
-            } else {
-                return 'pin yếu , bạn cần sạc điện thoại gấp !!!';
-            }
-        } else {
-            user = confirm('Bạn muốn sạc điện thoại ? ');
-            if (user == true) {
-                return 'Điện thoại'+this.name+ 'đang sạc';
-            }else {
-                return 'pin : '+this.getPin();
-            }
-        }
-    }
-
-
-
-    getPin() {
-        return this.pin;
+        this.pin = pin;
+        this.messageSent = messageSent
+        this.inbox = inbox;
+        this.draftMess = draftMess;
+        this.onOrOff = onOrOff;
     }
 
     getName() {
         return this.name;
     }
+    getPin() {
+        if (this.pin >= 0 || this.pin <= 100 ) {
+            return this.pin;
+        } else {
+            return 'Error';
+        }
+    }
+    getMessageSent() {
+        return this.messageSent;
+    }
+    getInbox() {
+        return this.inbox;
+    }
+    getOnOrOff() {
+        return this.onOrOff;
+    }
+    getDraftMess() {
+        return this.draftMess;
+    }
 
+
+    setInbox(message,obj2) {
+        obj2.inbox = obj2.inbox + message;
+    }
+
+    setDraftMess(mess) {
+        this.draftMess = this.draftMess + mess + '  ';
+    }
+
+    draftBox() {
+        if (this.getOnOrOff()=='On') {
+            this.pin--;
+            document.write('Thư nháp của '+ this.getName()+' : '+this.getDraftMess());
+        } else {
+            alert('Điện thoại đang tắt');
+        }
+    }
+
+    charging() {
+        if (this.getPin() < 20) {
+            alert('Pin yếu');
+            alert('Đang sạc điện thoại')
+        } else if(this.getPin() < 100) {
+            alert('Đang sạc điện thoại');
+        } else {
+            alert('Pin đầy');
+        }
+    }
+
+    checkPin() {
+        alert('Pin của '+this.getName()+' là : '+this.getPin() + ' %');
+    }
+
+    powerButton() {
+        this.pin--;
+        if (this.getOnOrOff()=='Off') {
+            return this.onOrOff = 'On';
+        } else {
+            return this.onOrOff = 'Off';
+        }
+    }
+
+    inBox() {
+        if (this.getOnOrOff()=='On') {
+            this.pin--;
+            alert('Tin nhắn gửi đến của '+ this.getName() + ' là : '+this.getInbox());
+        } else {
+            alert('Điện thoại đang tắt');
+        }
+    }
+
+    sentMessage(obj2) {
+        if (this.getOnOrOff()=='On') {
+            this.pin--;
+            let mess = prompt('Nhập tin nhắn : ');
+            let userConfirm = confirm('Bạn muốn gửi tin nhắn đang nhập ? ');
+            if (userConfirm==true) {
+                this.setInbox(mess,obj2);
+            }else {
+                this.setDraftMess(mess);
+            }
+        } else {
+            alert('Điện thoại đang tắt');
+        }
+    }
 }
+
+let nokia = new Mobile('nokia',100,'','','','Off');
+let ios = new Mobile('iphone' , 100 , '','','','Off');
+nokia.powerButton();
+ios.powerButton();
+nokia.sentMessage(ios);
+ios.inBox();
+nokia.checkPin();
+ios.checkPin();
